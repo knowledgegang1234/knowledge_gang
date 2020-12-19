4f7c5fd8770fe6eadc2f6c37913a6a546b82844c
Shubham Mishra <ramsmishra111@gmail.com>
2020-12-20 00:49:31 +0530

    Elevation UI animations

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,9 +5,11 @@ class TagsController < ApplicationController
   end
 
   def show
-    tag = Tag.friendly.find(params[:id])
-    blog_ids = tag.taggings.pluck(:blog_id)
-    @blogs = Blog.where(id: blog_ids)
+    @tag = Tag.friendly.find(params[:id])
+    @blogs_count = @tag.taggings.count
+    @followers_count = @tag.followers.count
+    blog_ids = @tag.taggings.pluck(:blog_id)
+    @blogs = Blog.where(id: blog_ids).page(params[:page]).per(12)
   end
 
 end
