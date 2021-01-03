0d9168229a0995f508e1d6aa7ecf988a18f9b41d
Swapnil Kumawat <swapnil.kumawat@zingoy.com>
2021-01-03 21:50:46 +0530

    precompile config changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.assets.precompile =  ['.js', '.css', '*.css.erb']
+  config.assets.precompile = ['blogs/*.js', 'channels/*.js', 'redactor_js/*.js', '*.js', 'blogs/*.css', 'login_signup/*.css', 'redactor_css/*.css']
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
