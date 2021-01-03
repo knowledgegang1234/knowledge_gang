955a15d10f8452d7816d52d67afbc962a6146a39
Swapnil Kumawat <swapnil.kumawat@zingoy.com>
2021-01-03 21:14:56 +0530

    user trending page css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def trending
     @trending_tags = Tagging.trending_tags_with_count
-    @trending_users = User.includes(:blogs).first(6)
+    @trending_users = User.includes(:blogs).first(5)
     @blogs = Blog.trending_blogs.page(params[:page]).per(12)
   end
 
