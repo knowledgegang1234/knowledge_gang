e4487e7914989ebbfa8ba2ae3b590e93d25a9c0c
Shubham Mishra <ramsmishra111@gmail.com>
2021-01-16 18:52:07 +0530

    email verification done

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+         :recoverable, :rememberable, :validatable, :confirmable,
          :omniauthable, omniauth_providers: %i[facebook google_oauth2]
 
   belongs_to :country, optional: true
